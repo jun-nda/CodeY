51a70e25a6b53811a45a20efc37eaddf8a425e68
ALGERLV <lvyao@camel4u.com>
2023-10-17 15:10:11 +0800

    场景

--- Assets/Scripts/PlayerControllor.cs
+++ Assets/Scripts/PlayerControllor.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 
 public class PlayerControllor : MonoBehaviour
 {
-    private CharacterController characterController;
+	private CharacterController characterController;
+
 	[SerializeField] private Camera camera;
 	public float sensitivity = 5.0f;
-    public float smoothing = 2.0f;
-    public float gravity;
-    public float jumpHeight;
+	public float smoothing = 2.0f;
+	public float gravity;
+	public float jumpHeight;
 
-    private Vector3 movementDirection;
-    private Vector2 mouseLook;
-    private Vector2 smoothV;
-    
-    public float WalkSpeed;
-    public float CurrentSpeed { get; private set; }
+	private Vector3 movementDirection;
+	private Vector2 mouseLook;
+	private Vector2 smoothV;
 
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+	public float WalkSpeed;
+	public float CurrentSpeed { get; private set; }
+
+	void Start()
+	{
+		characterController = GetComponent<CharacterController>();
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
 	public void Init(InputManager inputManager)
 	{
@@ -29,8 +30,16 @@ public class PlayerControllor : MonoBehaviour
 	}
 
 	void Update()
-    {
-        CurrentSpeed = WalkSpeed;
+	{
+		CameraControl();
+		PlayerMovementControl();
+	}
+
+	/// <summary>
+	/// 控制相机视角转动
+	/// </summary>
+	private void CameraControl()
+	{
 		Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
 		mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
@@ -40,33 +49,40 @@ public class PlayerControllor : MonoBehaviour
 
 		mouseLook.y = Mathf.Clamp(mouseLook.y, -60f, 90f);
 
-		camera.transform.localRotation = Quaternion.Euler(- mouseLook.y, mouseLook.x, 0);
+		camera.transform.localRotation = Quaternion.Euler(-mouseLook.y, mouseLook.x, 0);
+	}
 
+	/// <summary>
+	/// 控制角色移动
+	/// </summary>
+	private void PlayerMovementControl()
+	{
+		CurrentSpeed = WalkSpeed;
 		if (characterController.isGrounded)
 		{
-            var tmp_Horizontal = Input.GetAxis("Horizontal");
-            var tmp_Vertical = Input.GetAxis("Vertical");
-            movementDirection = camera.transform.TransformDirection(new Vector3(tmp_Horizontal, 0, tmp_Vertical)).normalized;
-            movementDirection.y = 0;
-            if (Input.GetButtonDown("Jump"))
-            {
-                movementDirection.y = jumpHeight;
-            }
+			var tmp_Horizontal = Input.GetAxis("Horizontal");
+			var tmp_Vertical = Input.GetAxis("Vertical");
+			movementDirection = camera.transform.TransformDirection(new Vector3(tmp_Horizontal, 0, tmp_Vertical)).normalized;
+			movementDirection.y = 0;
+			if (Input.GetButtonDown("Jump"))
+			{
+				movementDirection.y = jumpHeight;
+			}
 		}
 
-        movementDirection.y -= gravity * Time.deltaTime;
-        var tmp_Movement = CurrentSpeed * movementDirection;
-        characterController.Move(tmp_Movement);
+		movementDirection.y -= gravity * Time.deltaTime;
+		var tmp_Movement = CurrentSpeed * movementDirection;
+		characterController.Move(tmp_Movement);
 	}
 
-    private void HandleMouseMove(Vector3 vec)
+	private void HandleMouseMove(Vector3 vec)
 	{
-        //Debug.Log("HandleMouseMove" + vec);
+		//Debug.Log("HandleMouseMove" + vec);
 	}
 
-    void LateUpdate()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+	void LateUpdate()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
 }
