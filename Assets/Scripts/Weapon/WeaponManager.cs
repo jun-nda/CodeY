c1aec61fdead8e2b9759bde0eed3aa3c685c7da6
jun-nda <junnda@163.com>
2023-10-23 09:03:06 +0800

    武器系统，基类

---
+++ Assets/Scripts/Weapon/WeaponManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public Weapon mainWeapon;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            
+        }
+    }
+    
+}
