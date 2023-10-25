ce1ef4c89c37c40b2e6f79855f9b00e8ea8e5014
jun-nda <junnda@163.com>
2023-10-25 09:02:41 +0800

    开火逻辑

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,32 @@ using UnityEngine;
 public abstract class Weapon : MonoBehaviour,IWeapon {
 	
 	public abstract void OpenFire ();
+
+	public Transform m_MuzzlePos; //枪口
+	public Transform m_EjectionPos; // 子弹弹出位置
+
+	public ParticleSystem m_MuzzleParticle;
+	public ParticleSystem m_CasingParticle; // 弹壳特效
+
+	protected Animator m_GunAnimator;
+
+	public float m_FireRate;
+	protected float m_LastFireTime = 0f;
+
+	// 弹药数据相关
+	public int m_AmmoEach = 30;
+	public int m_AmmoAll = 120;
+	protected int m_CurrentAmmo;
+
+
+	protected void Awake () {
+		m_CurrentAmmo = m_AmmoEach;
+	}
+
+
+	protected abstract void Reload ();
+
+	protected bool IsAllowShooting () {
+		return Time.time - m_LastFireTime > 1 / m_FireRate;
+	}
 }
--- Assets/Scripts/Weapon/rifle.cs
+++ Assets/Scripts/Weapon/rifle.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using Input = UnityEngine.Windows.Input;
 
 public class rifle : Weapon {
 
 	public override void OpenFire () {
-
+		if (m_CurrentAmmo <= 0) return;
+		m_CurrentAmmo--;
+		// if (!IsAllowShooting()) return;
+		// MuzzleParticle.Play();
+		// CurrentAmmo -= 1;
+		//
+		// GunAnimator.Play("Fire", IsAiming ? 1 : 0, 0);
+		//
+		// FirearmsShootingAudioSource.clip = FirearmsAudioData.ShootingAudio;
+		// FirearmsShootingAudioSource.Play();
+		//
+		// CreateBullet();
+		// CasingParticle.Play();
+		// mouseLook.FiringForTest();
+		m_LastFireTime = Time.time;
 	}
 	
-	void shoot () {
+	protected override void  Reload () {
 		
 	}
+	
 	// Start is called before the first frame update
 	void Start () {
 
@@ -18,6 +34,8 @@ public class rifle : Weapon {
 
 	// Update is called once per frame
 	void Update () {
-
+		if (Input.GetMouseButton(0)) {
+			OpenFire();
+		}
 	}
 }
