52a39111524f219aca37906bb166bedba262c9e2
yujunda <yujunda@h3d.com.cn>
2024-01-03 18:59:53 +0800

    六边形地图2

--- Assets/Scripts/HexMap/HexMesh.cs
+++ Assets/Scripts/HexMap/HexMesh.cs
@@ -35,22 +35,37 @@ public class HexMesh : MonoBehaviour {
         meshCollider.sharedMesh = hexMesh;
     }
 	
+    // 根据6个方向来生成三角形
     void Triangulate (HexCell cell) {
-        Vector3 center = cell.transform.localPosition;
-        for (int i = 0; i < 6; i++) {
-            AddTriangle(
-                center,
-                center + HexMetrics.corners[i],
-                center + HexMetrics.corners[i + 1]
-            );
-            AddTriangleColor(cell.color);
+        for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
+            Triangulate(d, cell);
         }
     }
-    
+
+    void Triangulate(HexDirection direction, HexCell cell)
+    {
+        Vector3 center = cell.transform.localPosition;
+        AddTriangle(
+            center,
+            center + HexMetrics.GetFirstCorner(direction),
+            center + HexMetrics.GetSecondCorner(direction)
+        );
+        HexCell neighbor = cell.GetNeighbor(direction)  ?? cell;
+        Color edgeColor = (cell.color + neighbor.color) * 0.5f;
+        AddTriangleColor(cell.color, edgeColor, edgeColor);
+    }
+
+
     void AddTriangleColor (Color color) {
         colors.Add(color);
         colors.Add(color);
         colors.Add(color);
+    }
+    
+    void AddTriangleColor (Color c1, Color c2, Color c3) {
+        colors.Add(c1);
+        colors.Add(c2);
+        colors.Add(c3);
     }
     
     void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
